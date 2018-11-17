a3e20681972a031dc782595c4d588d4a486bf520
Lawrence Casely-Hayford <lawrencecasely-hayford@Lawrences-MacBook-Pro.local>
2018-11-17 22:43:52 +0100

    added test to it

--- test/controlsTest.js
+++ test/controlsTest.js
@@ -4,15 +4,11 @@ describe("Controls1", () => {
   it("App should return hello", () => {
     assert.equal("hello", "hello");
   });
-});
 
-describe("Controls2", () => {
   it("App should return hello2", () => {
     assert.equal("hello", "hello");
   });
-});
 
-describe("Controls3", () => {
   it("App should return hello3", () => {
     assert.equal("hello", "hello");
   });
