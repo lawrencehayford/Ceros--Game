4208dc69bd24a20586cb89408cfffc01fc01c4bf
Lawrence Casely-Hayford <lawrencecasely-hayford@Lawrences-MacBook-Pro.local>
2018-11-17 22:41:27 +0100

    added test to it

---
+++ test/controlsTest.js
@@ -0,0 +1,19 @@
+const assert = require("chai").assert;
+//const Controls = require("../js/controls");
+describe("Controls1", () => {
+  it("App should return hello", () => {
+    assert.equal("hello", "hello");
+  });
+});
+
+describe("Controls2", () => {
+  it("App should return hello2", () => {
+    assert.equal("hello", "hello");
+  });
+});
+
+describe("Controls3", () => {
+  it("App should return hello3", () => {
+    assert.equal("hello", "hello");
+  });
+});
